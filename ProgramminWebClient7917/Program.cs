30cf44e2e5805d0475809b0a9946451471e3c6b4
Matthew Peterson <matt2694@edu.eal.dk>
2017-09-07 10:55:57 +0200

    Add project files.

---
+++ ProgramminWebClient7917/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingWebClient7917
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                WebClient client = new WebClient();
+                //client.Headers.Add("Content-Type", "text/plain; charset=UTF-8");  // ekstra header info
+                //client.Encoding = Encoding.UTF8;                                  // ekstra om encoding
+                string Message = client.DownloadString("http://bjoerks.net/test/test.txt");
+                Console.WriteLine("Message:" + Message + ":end of Message");
+            }
+
+            {
+                WebClient client = new WebClient();
+                //client.Headers.Add("Content-Type", "text/htm; charset=UTF-8");     // ekstra header info
+                //client.Encoding = Encoding.UTF8;                                   // ekstra om encoding
+                string Message = client.DownloadString("http://bjoerks.net/Csharp/Opgaver/OpgaverHttp_ENG.htm");
+                Console.WriteLine("Message:" + Message + ":end of Message");
+            }
+            Console.ReadLine();         // vent på retur
+        }
+    }
+}
